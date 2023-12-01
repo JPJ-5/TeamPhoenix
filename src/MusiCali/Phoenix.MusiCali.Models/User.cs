56c6245213b5864b0a741d95a74474dee3c99024
Julie <97823380+Julie0126@users.noreply.github.com>
2023-12-01 00:22:36 -0800

    Created MusiCali codebase
    Made a rough structure of the codebase for everyone to add their code in

---
+++ src/MusiCali/Phoenix.MusiCali.Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Phoenix.MusiCali.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Additional properties, if needed
+
+    }
+}
---
+++ src/MusiCali/Wanderer.CraftVerify2.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Wanderer.CraftVerify2.Data
+{
+    public class Class1
+    {
+
+    }
+}
