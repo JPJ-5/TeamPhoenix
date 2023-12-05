cdb9d4b1871ff041f2ea02eec5725cdcfcabf8f2
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2023-12-05 11:32:44 -0800

    Update UserAuth.cs
    Added Registration Status and Logged in Methods

--- src/Phoenix.MusiCali.Models/UserAuth.cs
+++ src/Phoenix.MusiCali.Models/UserAuth.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -9,15 +9,50 @@ namespace Phoenix.MusiCali.Models
 {
     public class UserAuth
     {
-        public string Username { get; set; }
-        public string OTP { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? OTP { get; set; }
+        public string? Password { get; set; }
         public DateTime otpTimestamp { get; set; }
         public DateTime Timestamp { get; set; }
         public int FailedAttempts { get; set; }
         public DateTime? LastFailedAttemptTime { get; set; }
         public bool IsDisabled { get; set; }
         public bool IsAuth {  get; set; }
-        public string Salt {  get; set; }
+        public string? Salt {  get; set; }
+        private bool isRegistrationCompleted;
+
+
+
+
+        public RegistrationStatus GetRegistrationStatus()
+        {
+            if (isRegistrationCompleted)
+            {
+                return RegistrationStatus.Completed;
+            }
+            else
+            {
+                return RegistrationStatus.Incomplete;
+            }
+        }
+
+        public bool IsLoggedIn()
+        {
+            //Change Later with actual logic        
+            return true;
+        }
+
+        public bool HasActiveSession()
+        {
+            //Change Later with actual logic
+            return true;
+        }
     }
+
+        public enum RegistrationStatus
+        {
+            Incomplete,
+            Completed
+        }
 }
+
