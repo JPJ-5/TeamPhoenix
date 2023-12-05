39d626dee95690dbefa6660da1b6cb14ae2c81f1
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2023-12-04 19:18:53 -0800

    Update User.cs
    Added methods for logic checking if users are logged in or have active sessions

--- src/Phoenix.MusiCali.Models/User.cs
+++ src/Phoenix.MusiCali.Models/User.cs
@@ -1,4 +1,4 @@
-﻿namespace Phoenix.MusiCali.Models
+namespace Phoenix.MusiCali.Models
 {
     public class User
     {
@@ -10,7 +10,39 @@
         public string LastName { get; set; }
         public string userHash { get; set; }
 
+        public RegistrationStatus GetRegistrationStatus()
+        {
+            // Implement logic to determine the registration status
+            // This might involve checking if the user has completed the registration process
+            // You can replace this with your actual logic
+            return RegistrationStatus.Completed;
+        }
+
+        public bool IsLoggedIn()
+        {
+            // Implement logic to determine if the user is logged in
+            // This might involve checking if the user has an active session
+            // You can replace this with your actual logic
+            return true;
+        }
+
+        public bool HasActiveSession()
+        {
+            // Implement logic to determine if the user has an active session
+            // You can replace this with your actual logic
+            return true;
+        }
+
+        // Other methods...
+    }
+
+    public enum RegistrationStatus
+    {
+        Incomplete,
+        Completed,
+        // Add other statuses as needed
+    }
+
         // Additional properties, if needed
 
-    }
-}
+ }
