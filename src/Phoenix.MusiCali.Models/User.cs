8d86f9cc7a78b278d607d97c606aad43bea17ab5
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2023-12-04 19:27:36 -0800

    Update User.cs
    Removed some functions I added

--- src/Phoenix.MusiCali.Models/User.cs
+++ src/Phoenix.MusiCali.Models/User.cs
@@ -9,40 +9,4 @@ namespace Phoenix.MusiCali.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string userHash { get; set; }
-
-        public RegistrationStatus GetRegistrationStatus()
-        {
-            // Implement logic to determine the registration status
-            // This might involve checking if the user has completed the registration process
-            // You can replace this with your actual logic
-            return RegistrationStatus.Completed;
-        }
-
-        public bool IsLoggedIn()
-        {
-            // Implement logic to determine if the user is logged in
-            // This might involve checking if the user has an active session
-            // You can replace this with your actual logic
-            return true;
-        }
-
-        public bool HasActiveSession()
-        {
-            // Implement logic to determine if the user has an active session
-            // You can replace this with your actual logic
-            return true;
-        }
-
-        // Other methods...
-    }
-
-    public enum RegistrationStatus
-    {
-        Incomplete,
-        Completed,
-        // Add other statuses as needed
-    }
-
-        // Additional properties, if needed
-
  }
