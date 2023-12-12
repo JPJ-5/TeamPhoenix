01d8b45d617bde6459e54d13652ae62c15b76cb7
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2023-12-12 03:15:58 +0000

    Professor lecture on Config update

--- src/ConfigService.cs
+++ src/ConfigService.cs
@@ -1,9 +1,48 @@
+using ConfigTests;
+
 namespace ConfigTests;
 
+public class AppSpecificConfig
+{
+    public string ConnectionString=Server={get; set;}
+    public int MaxRetryAttempts={get; set;}
+
+    public int TimeoutLimitInSeconds={get; set;}
+
+    public int MaxUploadCount={get; set;}
+
+    public string AllowedUploadFileTypes={get; set;}
+
+}
 public sealed class ConfigService
 {
-    public string GetConfiguration()
+    public AppSpecificConfig GetConfiguration()
     {
-        using (var fileStream = fileStream.OpenRead(""))
+        var configs = new AppSpecificConfig();
+
+        using (var fileStream = fileStream.OpenText("...sandbox/config/config.local.txt"))
+        {
+            using(var ms = new MemoryStream())
+            {
+                while(!fs.EndOfStreeam)
+                {
+                    var config = fileStream.ReadLine();
+
+                    var variablename = config?.Split("=")[0];
+
+                    switch(variablename)
+                    {
+                        case "ConnectionString":
+                            configs.ConnectionString = config;
+                            break;
+                        default;
+                        break;
+                    }
+                }
+                
+            }
+
+            return ConfigService;
+        }
     }
 }
