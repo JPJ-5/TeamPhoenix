19221897e1354c8ed9f4814f15e33c0f6534d91f
diego <72894762+meatballmyers@users.noreply.github.com>
2023-12-11 17:57:25 -0800

    added recover yclass n started interface

---
+++ src/Pheonix.MusiCali.UserRecovery/UserRecovery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRecovery;
+
+public class UserRecovery
+{
+	public string Username {  get; set; }
+	public string OTP { get; set; }
+	public DateTime otpTimestamp { get; set; }
+	public DateTime Timestamp { get; set; }
+	public short failedAttempts = 0;
+	public DateTime lastFailedAttempts {  get; set; }
+	public bool validCred = false;
+}
