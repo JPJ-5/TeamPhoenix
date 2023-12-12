1f9c3f75e79d769971e44521e5ddef90d46120da
diego <72894762+meatballmyers@users.noreply.github.com>
2023-12-11 18:24:16 -0800

    interface stuff
    the color is weird so idk if its workin right

---
+++ src/Phoenix.MusiCali.UserRecovery/IRecovery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Phoenix.MusiCali.UserRecovery;
+
+public interface IRecovery
+{
+    public bool recover(string username, string OTP);
+
+    public bool isValidUsername(string username);
+
+    public bool isValidOTP(string OTP);
+
+    public bool validateCred(string username, string OTP);
+}
---
+++ src/Phoenix.MusiCali.UserRecovery/UserRecovery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Phoenix.MusiCali.UserRecovery;
+
+public class UserRecovery
+{
+	public string Username {  get; set; }
+	public string OTP { get; set; }
+	public DateTime otpTimestamp { get; set; }
+	public DateTime Timestamp { get; set; }
+	public short failedAttempts = 0;
+	public DateTime lastFailedAttempts {  get; set; }
+	public bool validCred = false;
+}
+
+public class Recovery : IRecovery
+{
+	public bool recover(Username, OTP)
+	{
+		return true;
+	}
+
+	public bool isValidUsername(string username) 
+	{  
+		return true;
+	}
+
+	public bool isValidOTP(string OTP)
+	{
+		return true;
+	}
+
+	public bool validateCred(string username, string OTP)
+	{
+		return true;
+	}
+}
