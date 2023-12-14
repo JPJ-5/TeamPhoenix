85c5fe710970ee761ce0072bf0093cbf4477ad94
Julie <97823380+Julie0126@users.noreply.github.com>
2023-12-13 17:26:47 -0800

    updated to add principal and fixed some logic

--- src/Phoenix.MusiCali.Models/UserAuth.cs
+++ src/Phoenix.MusiCali.Models/UserAuth.cs
@@ -10,14 +10,21 @@ namespace Phoenix.MusiCali.Models
     public class UserAuth
     {
         public string Username { get; set; }
+        public string Salt { get; set; }
         public string OTP { get; set; }
         public string Password { get; set; }
         public DateTime otpTimestamp { get; set; }
         public DateTime Timestamp { get; set; }
         public int FailedAttempts { get; set; }
-        public DateTime? LastFailedAttemptTime { get; set; }
+        public DateTime FirstFailedAttemptTime { get; set; }
         public bool IsDisabled { get; set; }
         public bool IsAuth {  get; set; }
-        public string Salt {  get; set; }
+        public bool IsEnabled {  get; set; }
+        public bool EmailSent { get; set; }
+    }
+
+    public UserAuth(string username, string otp, string password, DateTime otpTime, DateTime time, int failed, DateTime last, bool disabled, bool auth, bool email, string salt)
+    {
+        
     }
 }
