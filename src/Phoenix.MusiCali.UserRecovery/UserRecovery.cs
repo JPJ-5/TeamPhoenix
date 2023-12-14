63b6d9ce3dd2f514419e107c2fd10c153e297521
diego <72894762+meatballmyers@users.noreply.github.com>
2023-12-14 03:43:06 -0800

    tinkering with recovery methods
    can probably just call the auth validation methods for username/otp

--- src/Phoenix.MusiCali.UserRecovery/UserRecovery.cs
+++ src/Phoenix.MusiCali.UserRecovery/UserRecovery.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Phoenix.MusiCali.UserRecovery;
 
 public class UserRecovery
@@ -8,25 +9,40 @@ public class UserRecovery
 	public string OTP { get; set; }
 	public DateTime otpTimestamp { get; set; }
 	public DateTime Timestamp { get; set; }
-	public short failedAttempts = 0;
+	public int failedAttempts = 0;
 	public DateTime lastFailedAttempts {  get; set; }
 	public bool validCred = false;
 }
 
 public class Recovery : IRecovery
 {
-	public bool recover(Username, OTP)
+	public Result recover(string username)
 	{
-		return true;
+		UserRecovery userRecovery = new UserRecovery();
+		return new Result;
 	}
 
 	public bool isValidUsername(string username) 
-	{  
+	{
+		//compares given string to username rules
+		//Minimum of 8 characters
+		//a - z(case insensitive)
+		//0 - 9
+		//Allow the following special characters: . – @
+		if(username.Length < 8 | username.Length > 30) {  return false; }
+		//core reqs doesn't mention a max length for username or OTP, weirdly enough
+		for(var i=0; i < username.Length; i++)
+		{
+			if (username[i] ==)
+		}
+		//see if i can just call the same method from auth
 		return true;
 	}
 
 	public bool isValidOTP(string OTP)
 	{
+		//expires within 2 minutes
+		//minimum eight characters, a-z, A-Z, 0-9
 		return true;
 	}
 
