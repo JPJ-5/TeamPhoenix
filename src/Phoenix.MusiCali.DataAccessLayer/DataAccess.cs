220ca3aab0757c3d27abe7ec24ac3482db8dcd2a
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2024-01-18 15:08:08 -0800

    Update DataAccess.cs
    Removed Logging Implementation. Gonna move it to logging

--- src/Phoenix.MusiCali.DataAccessLayer/DataAccess.cs
+++ src/Phoenix.MusiCali.DataAccessLayer/DataAccess.cs
@@ -47,24 +47,6 @@ namespace Phoenix.MusiCali.DataAccesslayer
             return password.Count(char.IsDigit) == 1;
         }
 
-        public Result createLog(DateTime timestamp, string logLevel, string logCategory, string context)
-        {
-            var result = new Result();
-
-            try
-            {
-                Console.WriteLine($"Logging - Timestamp: {timestamp}, Level: {logLevel}, Category: {logCategory}, Context: {context}");
-                result.hasError = false;
-            }
-            catch (Exception ex)
-            {
-                result.hasError = true;
-                result.errorMessage = ex.Message;
-            }
-
-            return result;
-        }
-
         public Result createUser(string firstName, string lastName, string username, string email, DateTime dateOfBirth)
         {
             var result = new Result();
@@ -122,20 +104,5 @@ namespace Phoenix.MusiCali.DataAccesslayer
         public int Users { get; private set; } // Updated to a private set
 
         // Additional methods or properties can be added here as needed
-    }
-
-    public class Logger
-    {
-        private DataAccess dao; // Create DataAccessObject once for Logger
-
-        public Logger()
-        {
-            dao = new DataAccess();
-        }
-
-        public Result log(string logLevel, string logCategory, string context)
-        {
-            return dao.createLog(DateTime.UtcNow, logLevel, logCategory, context);
-        }
     }
 }
