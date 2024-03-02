c86b2a5605040a96c4f7e401dcec7b08ad196162
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2024-03-01 21:43:10 -0800

    Create Auth.js

---
+++ src/FrontEnd/Auth.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.post('/login', (req, res) => {
+    // Perform user authentication here
+    // If authentication is successful, redirect the user to the user modification page
+    res.redirect('/UserSettings.html');
+});
+
+// Serve the user modification page
+app.get('/UserSettings.html', (req, res) => {
+    res.sendFile(__dirname + '/UserSettings.html');
+});
+
+// Serve other static files (CSS, JavaScript, etc.)
+app.use(express.static(__dirname));
+
+// Start the server
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
