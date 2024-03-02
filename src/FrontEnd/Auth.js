a803c0bf5fc3cf30acdb36b8923f64d31dda368c
ShaneKerr2001 <55069129+ShaneKerr2001@users.noreply.github.com>
2024-03-01 21:46:22 -0800

    Update Auth.js

--- src/FrontEnd/Auth.js
+++ src/FrontEnd/Auth.js
@@ -4,12 +4,12 @@ const app = express();
 app.post('/login', (req, res) => {
     // Perform user authentication here
     // If authentication is successful, redirect the user to the user modification page
-    res.redirect('/UserSettings.html');
+    res.redirect('/UserModify.html');
 });
 
 // Serve the user modification page
-app.get('/UserSettings.html', (req, res) => {
-    res.sendFile(__dirname + '/UserSettings.html');
+app.get('/UserModify.html', (req, res) => {
+    res.sendFile(__dirname + '/UserModify.html');
 });
 
 // Serve other static files
