56ef3889eefc64f7c56ba6a4b783e8b407b3f9e8
Parth <officialparththanki@gmail.com>
2024-03-03 20:20:31 -0800

    Perfectly work code and julie fixed userclaims table.

---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.Services/ModifyUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+using ModifyUserDao = TeamPhoenix.MusiCali.DataAccessLayer.ModifyUser;
+
+namespace TeamPhoenix.MusiCali.Services
+{
+    public class ModifyUser
+    {
+
+        public static bool modifyProfile(UserProfile userP)
+        {
+
+            if (!isNameValid(userP.FirstName))
+            {
+
+                throw new InvalidDataException();
+            }
+
+            if (!isNameValid(userP.LastName))
+            {
+
+                throw new InvalidDataException();
+            }
+
+            try
+            {
+                ModifyUserDao.UpdateProfile(userP);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception($"Error updating UserProfile: {ex.Message}");
+            }
+
+            return true;
+        }
+
+        public static bool isNameValid(string name)
+        {
+            return Regex.IsMatch(name, @"^[a-zA-Z]+$");
+        }
+
+    }
+}
---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.Security/OtpService.cs
@@ -0,0 +1,36 @@
+﻿//using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+//using System.Collections;
+//using _authnDao = TeamPhoenix.MusiCali.DataAccessLayer.Authentication;
+
+//namespace TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.Security
+//{
+//    public class OtpService
+//    {
+//        public List<Object> GenerateNewOTP(string username)
+//        {
+//            try
+//            {
+//                List<object> otpInfo = new List<object>();
+//                AuthResult authResult = _authnDao.findUsernameInfo(username);
+//                DateTime currentTime = DateTime.Now;
+//                if (currentTime >= authResult.userA.otpTimestamp.AddMinutes(2))
+//                {
+//                    authResult.userA.OTP = "";
+//                    authResult.userA.otpTimestamp = currentTime;
+//                }
+//                else
+//                {
+//                    otpInfo.Add(authResult.userA.Username);
+//                    otpInfo.Add(authResult.userA.OTP);
+//                    otpInfo.Add(authResult.userA.otpTimestamp);
+//                    return otpInfo;
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"Error: {ex}");
+//                return null;
+//            }
+//        }
+//    }
+//}
