e4b6288dec03e2b7b6491a336f881bd19c186def
diego <72894762+meatballmyers@users.noreply.github.com>
2024-03-18 16:52:05 -0700

    updating modifyusertest
    i'll either need to change modifyuser or something because rn the "getprofileinformation" function returns an anonymous type which afaict is unworkable as a returned object

--- src/Latest Backend Project/TeamPhoenix.MusiCali.Tests/DataAccessLayer/ModifyUserTest.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.Tests/DataAccessLayer/ModifyUserTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MySql.Data.MySqlClient;
 using dao = TeamPhoenix.MusiCali.DataAccessLayer.ModifyUser;
-using uc = TeamPhoenix.MusiCali.Services.UserCreation;
+using uC = TeamPhoenix.MusiCali.Services.UserCreation;
 using TeamPhoenix.MusiCali.DataAccessLayer.Models;
 using TeamPhoenix.MusiCali.DataAccessLayer;
 
@@ -17,15 +17,32 @@ namespace TeamPhoenix.MusiCali.Tests.DataAccessLayer
         public void UpdateProfile_ShouldUpdateUserProfile()
         {
             // Arrange
-            DateTime dob = new DateTime(2001, 01, 26);
-            uc.RegisterUser("joshuareyes@gmail.com", dob, "Julie0126", "Julie", "Reyes", "What is your pets name", "Ace");
-            UserProfile userProfile = new UserProfile("julie0126", "Julie", "Reyes", dob);
+            string email = "test@example.com";
+            DateTime dateOfBirth = new DateTime(1990, 1, 1);
+            string username = "testuser";
+            string updatedUsername = "testuser1";
+            string fname = "John";
+            string lname = "Doe";
+            string backupEmail = "";
+            uC.RegisterNormalUser(email, dateOfBirth, username, backupEmail);
+            UserProfile userProfile = new UserProfile(username, fname, lname, dateOfBirth);
 
             // Act
             dao.UpdateProfile(userProfile);
 
+            //getuserinformation returnes an unclassed object
+            /*
+             * var updatedProfile = new {
+                FirstName = "",
+                LastName = "",
+                DateOfBirth = "",
+                Email = "",
+                UserStatus = "",
+                UserRole =""
+            };
+            */
             // Assert - Check if the profile is updated
-            UserProfile updatedProfile = dao.GetUserProfile("julie0126");
+            var updatedProfile = dao.GetUserInformation(username);
             Assert.AreEqual(userProfile.FirstName, updatedProfile.FirstName);
             Assert.AreEqual(userProfile.LastName, updatedProfile.LastName);
             Assert.AreEqual(userProfile.DOB, updatedProfile.DOB);
