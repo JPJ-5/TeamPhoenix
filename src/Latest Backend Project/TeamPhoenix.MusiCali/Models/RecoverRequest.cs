17f0c60f9f407dd6b20dafb8e016eba44b8fa8f8
vangriff00 <vankhuong0307@gmail.com>
2024-03-24 00:58:17 -0700

    Upload Correct Folder

--- src/Latest Backend Project/TeamPhoenix.MusiCali/Models/RecoverRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeamPhoenix.MusiCali.Models
-{
-    public class RecoverRequest
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string GivenOTP { get; set; } = string.Empty;
-
-    }
-}
