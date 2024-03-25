5814cadf42ea6fa4232741d957371cf37eccc003
diego <72894762+meatballmyers@users.noreply.github.com>
2024-03-25 16:08:02 -0700

    slight userdeletion modification
    made method static so result was workable

--- src/Latest Backend Project/TeamPhoenix.MusiCali.Tests/Services/UserDeletionTest.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.Tests/Services/UserDeletionTest.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamPhoenix.MusiCali.DataAccessLayer;
+using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+using TeamPhoenix.MusiCali.Services;
 using uC = TeamPhoenix.MusiCali.Services.UserCreation;
+using uD = TeamPhoenix.MusiCali.Services.UserDeletion;
 
 namespace TeamPhoenix.MusiCali.Tests.Services
 {
@@ -17,6 +18,12 @@ namespace TeamPhoenix.MusiCali.Tests.Services
             string backupEmail = "backupdeleteTest@example.com";
             DateTime dateOfBirth = new DateTime(1990, 1, 1);
             string username = "deletetestuser";
+
+            bool createRes = uC.RegisterNormalUser(email, dateOfBirth, username, backupEmail);
+            Assert.IsTrue(createRes);
+
+            Result deleteRes = uD.DeleteAccount(username);
+
         }
     }
 }
