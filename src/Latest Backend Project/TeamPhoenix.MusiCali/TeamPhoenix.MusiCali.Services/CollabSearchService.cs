fe16db66d47e45c1e11087f739fd4b7ac573687d
ShaneKerr2001 <skerr2001@gmail.com>
2024-04-01 00:19:51 -0700

    Added in CollabSearch Feature

---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.Services/CollabSearchService.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.DataAccessLayer;
+
+namespace TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.Services
+{
+    public class CollabSearchService
+    {
+        private readonly CollabSearchDataAccess _collabSearchDataAccess;
+
+        public CollabSearchService(CollabSearchDataAccess collabSearchDataAccess)
+        {
+            _collabSearchDataAccess = collabSearchDataAccess;
+        }
+
+        public async Task<DataTable> SearchUsersAsync(string username)
+        {
+            return await _collabSearchDataAccess.SearchUsersAsync(username);
+        }
+
+        public async Task UpdateUserVisibilityAsync(string username, string visibility)
+        {
+            await _collabSearchDataAccess.UpdateUserVisibilityAsync(username, visibility);
+        }
+    }
+}
+
