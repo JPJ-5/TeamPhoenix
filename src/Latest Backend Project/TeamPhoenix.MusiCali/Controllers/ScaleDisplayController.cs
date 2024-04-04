0710cb978b16716c381c720d119ad7563b20bdbd
Kihambo <48707537+Kihambo@users.noreply.github.com>
2024-04-03 21:45:47 -0700

    fixed scale display code with one error in ScaleDisplayController, however.

---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/Controllers/ScaleDisplayController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using TeamPhoenix.MusiCali.DataAccessLayer;
+using scaleDisplay = TeamPhoenix.MusiCali.Services.ScaleDisplay;
+
+namespace TeamPhoenix.MusiCali.Controllers
+{
+    [ApiController]
+    [Route("api/ScaleDisplay")]
+    
+    public class ScaleDisplayController : ControllerBase
+    {
+        private readonly ScaleDisplay scaleDisplay;
+
+        [HttpPost("api/logScaleDisplay")]
+        public IActionResult logScaleUses([FromBody]string username)
+        {
+            try{
+                // Initialize the data access layer
+                scaleDisplay = new ScaleDisplay();
+                if(scaleDisplay.ScaleDisplayLoggingService(username)){
+                    return OK("Logged Scale Usage");
+                }
+                else{
+                    throw new Exception("Log Scale Uses Failed");
+                }
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex.toString());
+                return BadRequest("Log Scale Uses Failed");
+            }
+        }
+    }
+}
+
