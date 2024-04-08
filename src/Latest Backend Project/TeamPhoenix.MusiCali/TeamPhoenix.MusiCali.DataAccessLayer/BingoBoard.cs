6fb036af901ed3c3e65735da8cd31d8f2d073eaf
diego <72894762+meatballmyers@users.noreply.github.com>
2024-04-08 15:50:57 -0700

    Update BingoBoard.cs

--- src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.DataAccessLayer/BingoBoard.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.DataAccessLayer/BingoBoard.cs
@@ -13,14 +13,14 @@ namespace TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.DataAccessLayer
             string userHash;
 
             string connectionString = "Server=3.142.241.151;Database=MusiCali;User ID=julie;Password=j1234;";
-            string viewGigSummarySql = "SELECT PosterUsername, GigName, GigDateTime FROM Gig WHERE GigVisibility = TRUE ORDER BY GigDateTime LIMIT @giglimit";
+            string viewGigSummarySql = "SELECT PosterUsername, GigName, GigDateTime FROM Gig WHERE GigVisibility = TRUE ORDER BY GigDateTime LIMIT @GigLoadLimit";
 
             using (var connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
                 using (var command = new MySqlCommand(viewGigSummarySql, connection))
                 {
-
+                    command.Parameters.AddWithValue("@GigLoadLimit", numberOfGigs);
                 }
             }
 
