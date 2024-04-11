4ad9c40d52ba1b5c9c2567a670f2600d9aad4fa3
diego <72894762+meatballmyers@users.noreply.github.com>
2024-04-10 19:02:38 -0700

    Update BingoBoard.cs
    added alt retrieval method to only include gigs from a specific date

--- src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.DataAccessLayer/BingoBoard.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.DataAccessLayer/BingoBoard.cs
@@ -70,5 +70,61 @@ namespace TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.DataAccessLayer
                 }
             }
         }
+
+        public static List<GigSummary>? ViewGigSummaryByDate(string currentUsername, DateTime gigDateTime)
+        {
+            string level;
+            string category;
+            string context;
+            string userHash;
+
+            List<GigSummary> gigs = new List<GigSummary>();
+
+            string connectionString = "Server=3.142.241.151;Database=MusiCali;User ID=julie;Password=j1234;";
+            string viewGigSummarySql = "SELECT PosterUsername, GigName, GigDateTime FROM Gig WHERE GigVisibility = TRUE AND AND GigDateTime = @GigDateTime ORDER BY GigDateTime";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new MySqlCommand(viewGigSummarySql, connection))
+                {
+                    command.Parameters.AddWithValue("@GigDateTime", gigDateTime);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        //should add every gig summary to a gigsummary list to be returned
+                        while (reader.Read())
+                        {
+                            var newGig = new GigSummary(
+                                reader["PosterUsername"].ToString() ?? string.Empty,
+                                reader["GigName"].ToString() ?? string.Empty,
+                                Convert.ToDateTime(reader["GigDateTime"])
+                                );
+                            gigs.Add(newGig);
+                            //reader.NextResult();
+                        }
+                        if (gigs.Count == 0)
+                        {
+                            userHash = rU.GetUserHash(currentUsername);
+                            level = "Info";
+                            category = "View";
+                            context = "Failed to retrieve gigs.";
+                            _loggerCreation.CreateLog(userHash, level, category, context);
+                            return null;
+                        }
+   
+                        else
+                        {
+                            userHash = rU.GetUserHash(currentUsername);
+                            level = "Info";
+                            category = "View";
+                            context = $"{gigs.Count} gigs successfully retrieved from database.";
+                            _loggerCreation.CreateLog(userHash, level, category, context);
+                        }
+                        return gigs;
+                    }
+                }
+            }
+        }
     }
 }
