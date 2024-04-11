1aa1b924a26426db78914f4306b5bf3e225baa0e
diego <72894762+meatballmyers@users.noreply.github.com>
2024-04-10 18:31:56 -0700

    Update BingoBoard.cs

--- src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.Services/BingoBoard.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.Services/BingoBoard.cs
@@ -1,6 +1,23 @@
-﻿namespace TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.Services
+﻿using TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.DataAccessLayer.Models;
+using _dao = TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.DataAccessLayer.BingoBoard;
+
+namespace TeamPhoenix.MusiCali.TeamPhoenix.MusiCali.Services
 {
     public class BingoBoard
     {
+        public static List<GigSummary>? ViewMultGigSummary(ushort numberOfGigs, string username)
+        {
+            try
+            {
+                return _dao.ViewGigSummary(numberOfGigs, username);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving gig information: {ex.Message}");
+                throw;
+            }
+
+            return null;
+        }
     }
 }
