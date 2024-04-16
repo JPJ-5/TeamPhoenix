8d38d6cfd66d6de65d826aba7bc828b4b184b09f
JPJ-5 <jjitsiripol@gmail.com>
2024-04-15 18:53:29 -0700

    Remove un-updated code
    Remove un-updated code

--- src/Latest Backend Project/TeamPhoenix.MusiCali/TeamPhoenix.MusiCali.DataAccessLayer/Models/GigFindModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TeamPhoenix.MusiCali.DataAccessLayer.Models
-{
-    public class GigFindModel
-    {
-        public string Username { get; set; } = string.Empty;
-        public DateTime DateOfGig { get; set; }
-    }
-}
