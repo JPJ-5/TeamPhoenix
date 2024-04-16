e51fee475e8c3748ad109f8836f1ebc9ecb8fa2e
Julie <97823380+Julie0126@users.noreply.github.com>
2024-04-15 18:01:46 -0700

    updating view to include username and fixing structure and adding good notation

--- src/Latest Frontend Project/TestUI/wwwroot/js/ArtistPortfolio.js
+++ src/Latest Frontend Project/TestUI/wwwroot/js/ArtistPortfolio.js
@@ -48,14 +48,23 @@ function playMedia(slot) {
 
 function displayArtistProfile(portfolioInfo) {
     const infoSlot = document.getElementById(`info-upload`);
+    var activeUsername = sessionStorage.getItem('username');
 
+    //Creating the Username Display
+    const infoParagraph = document.createElement('p');
+    infoParagraph.innerHTML = `<strong>${activeUsername}`;
+    infoSlot.appendChild(infoParagraph);
+
+    //Display for other occupation with selection boxes
     if (portfolioInfo[`occupation`]){
         const info = portfolioInfo[`occupation`];
     
+        //paragraphs for genre and description if existing
         const infoParagraph = document.createElement('p');
         infoParagraph.innerHTML = `<strong>Occupation: </strong>${info}`;
         infoSlot.appendChild(infoParagraph);
 
+        //delete button for existing picture
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete info';
         deleteButton.addEventListener('click', function() {
@@ -64,50 +73,54 @@ function displayArtistProfile(portfolioInfo) {
         infoSlot.appendChild(deleteButton);
 
     } else {
+        //Creating select dropdown for occupation when null
         const options = ['Composer', 'Arranger', 'Producer', 'Instrumentalist', 'None'];
+
+        const infoParagraph = document.createElement('p');
+        infoParagraph.innerHTML = `<strong>Occupation: </strong>No occupation yet`;
+        infoSlot.appendChild(infoParagraph);
+
+        //adding all the options into the dropwown selection
+        const dropdown = document.createElement('select');
+        dropdown.id = `occupation-input`;
+        options.forEach(optionText => {
+            const option = document.createElement('option');
+            dropdown.appendChild(option);
+        });
+        infoParagraph.appendChild(dropdown);
+
+        //creating upload button for user to activate upload
         const uploadButton = document.createElement('button');
         uploadButton.textContent = `Upload Occupation`;
         uploadButton.addEventListener('click', function() {
             triggerInfoInput(`occupation`);
         });
-
-        const infoParagraph = document.createElement('p');
-        infoParagraph.innerHTML = `<strong>Occupation: </strong>No occupation yet`;
-
-        const dropdown = document.createElement('select');
-        dropdown.id = `occupation-input`;
-        options.forEach(optionText => {
-            const option = document.createElement('option');
-            option.value = optionText.toLowerCase();
-            option.text = optionText.charAt(0).toUpperCase() + optionText.slice(1);
-            dropdown.appendChild(option);
-        });
-        infoParagraph.appendChild(dropdown);
-        infoSlot.appendChild(infoParagraph);
         infoSlot.appendChild(uploadButton);
     }
 
+    //creating display for bio and location ifo
     var artistInfo = ["bio", "location"];
     for (let i = 0; i < 2; i ++) {
         if (!portfolioInfo[`${artistInfo[i]}`]) {
 
+            const infoParagraph = document.createElement('p');
+            infoParagraph.innerHTML = `<strong>${artistInfo[i]}: </strong>No ${artistInfo[i]} yet`;
+            infoSlot.appendChild(infoParagraph);
+            
+            //input field created for info that it missing to be uplaoded when upload button is clicked
+            const portfolioInput = document.createElement('input');
+            portfolioInput.type = 'text';
+            portfolioInput.id = `${artistInfo[i]}-input`;
+            portfolioInput.placeholder = `Enter your desired ${artistInfo[i]}`;
+            infoSlot.appendChild(portfolioInput);
+            
             const uploadButton = document.createElement('button');
             uploadButton.textContent = `Upload ${artistInfo[i]}`;
             uploadButton.addEventListener('click', function() {
                 triggerInfoInput(`${artistInfo[i]}`);
             });
-
-            const infoParagraph = document.createElement('p');
-            infoParagraph.innerHTML = `<strong>${artistInfo[i]}: </strong>No ${artistInfo[i]} yet`;
-            
-            const portfolioInput = document.createElement('input');
-            portfolioInput.type = 'text';
-            portfolioInput.id = `${artistInfo[i]}-input`;
-            portfolioInput.placeholder = `Enter your desired ${artistInfo[i]}`;
-
-            infoSlot.appendChild(infoParagraph);
-            infoSlot.appendChild(portfolioInput);
             infoSlot.appendChild(uploadButton);
+
         } else {
             const info = portfolioInfo[`${artistInfo[i]}`];
         
@@ -126,9 +139,13 @@ function displayArtistProfile(portfolioInfo) {
 
     // Track the number of elements that need to be loaded
     let elementsToLoad = 0;
-
+    
+    //Creating display for the profile picture
     if (portfolioInfo[`file0`]) {
+        const mediaSlot = document.getElementById(`file-upload-0`);
         elementsToLoad++;
+
+        //creating image using extension from backend to create right type
         const image = document.createElement('img');
         var ext = portfolioInfo[`file${0}Ext`];
         const filePath = 'data:image/' + ext + ';base64,' + portfolioInfo[`file0`];
@@ -137,13 +154,13 @@ function displayArtistProfile(portfolioInfo) {
             elementsToLoad--;
             checkAllLoaded();
         };
+        mediaSlot.appendChild(image);
+
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete Profile Pic';
         deleteButton.addEventListener('click', function() {
             deleteFile(0);
         });
-        const mediaSlot = document.getElementById(`file-upload-0`);
-        mediaSlot.appendChild(image);
         mediaSlot.appendChild(deleteButton);
     } else {
         const uploadButton = document.createElement('button');
@@ -155,13 +172,13 @@ function displayArtistProfile(portfolioInfo) {
         mediaSlot.appendChild(uploadButton);
     }
 
-    // Set media sources based on file type for each media slot
+    // Creating display for media sources based on file type for each media slot, Genre and Description included
     for (let i = 1; i <= 5; i++) {
         const fileContent = portfolioInfo[`file${i}`];
         const mediaSlot = document.getElementById(`slot-${i}`);
 
         if (fileContent == null) {
-            //If null then we load upload buttons for media and set genre and desc to display lack media
+            //If null then we load upload button and input for info and media and set genre and desc to display lack of media
             const genreParagraph = document.createElement('p');
             genreParagraph.innerHTML = `<strong>Genre: </strong>No file yet`;
             mediaSlot.appendChild(genreParagraph);
@@ -176,7 +193,7 @@ function displayArtistProfile(portfolioInfo) {
                 triggerFileInput(i);
             });
 
-            //Gotta make input boxes for Genre and Desc input as well
+            //input boxes for Genre and Desc input if null
             const genreInput = document.createElement('input');
             genreInput.type = 'text';
             genreInput.id = `genre-input-${i}`;
@@ -190,7 +207,7 @@ function displayArtistProfile(portfolioInfo) {
             mediaSlot.appendChild(descInput);
             mediaSlot.appendChild(uploadButton);
         } else {
-            //Create in Genre and Desc paragraphs when not null
+            //Create display for Genre and Desc paragraphs when not null
             const genre = portfolioInfo[`file${i}Genre`] || 'N/A';
             const desc = portfolioInfo[`file${i}Desc`] || 'N/A';
         
@@ -208,13 +225,14 @@ function displayArtistProfile(portfolioInfo) {
             const supportedAudioFormats = ['.mp3', '.wav'];
             const supportedVideoFormats = ['.mp4'];
         
-            //Dependedt on the extension type it will construct media object to play
+            //check for correct extension and for which exact type to contruct media
             if (supportedAudioFormats.includes(extension) || supportedVideoFormats.includes(extension)) {
                 elementsToLoad++;
                 const media = extension === '.mp3' ? document.createElement('audio') : document.createElement('video');
                 media.src = 'data:audio/' + extension.substring(1) + ';base64,' + fileContent;
                 media.controls = true;
                 media.style.display = 'block';
+                //calling checkAllLoaded to make sure everything is ready before showing
                 media.onloadeddata = () => {
                     elementsToLoad--;
                     checkAllLoaded();
@@ -229,7 +247,7 @@ function displayArtistProfile(portfolioInfo) {
                 });
                 mediaSlot.appendChild(deleteButton);
         
-                // Add play button if it is audio
+                // Add play button if it is audio only
                 if (extension === '.mp3') {
                     const playButton = document.createElement('button');
                     playButton.textContent = 'Play';
@@ -242,6 +260,7 @@ function displayArtistProfile(portfolioInfo) {
         }      
     }
 
+    //check no more elements loading before displaying
     const checkAllLoaded = () => {
         if (elementsToLoad === 0) {
             // Once all elements are loaded, display the profile view
@@ -253,9 +272,10 @@ function displayArtistProfile(portfolioInfo) {
     };
 }
 
+//function to call api to delete file along with genre and description
 function deleteFile(slot){
     var activeUsername = sessionStorage.getItem('username');
-    // Send POST request to upload API endpoint
+    // Send POST request to delete API endpoint
     deleteForm = new FormData();
     deleteForm.append('Username', activeUsername);
     deleteForm.append('SlotNumber', slot);
@@ -272,18 +292,19 @@ function deleteFile(slot){
     })
     .then(responseText => {
         console.log(responseText); // Log success message
-        document.getElementById('enter-artistPortfolio').click();
+        document.getElementById('enter-artistPortfolio').click(); //reload page once data is changed in ayway
     })
     .catch(error => {
         console.error('Error:', error); // Log error message
     });
 }
 
+//function to call api to delete section in profile info
 function deleteInfo(section) {
     var activeUsername = sessionStorage.getItem('username');
     const deleteSection = [activeUsername, `${section}`];
 
-    // Send POST request to upload API endpoint
+    // Send POST request to delete info API endpoint
     fetch('http://localhost:8080/ArtistPortfolio/api/delInfoApi', {
         method: 'POST',
         headers: {
@@ -300,25 +321,27 @@ function deleteInfo(section) {
     })
     .then(responseText => {
         console.log(responseText); // Log success message
-        document.getElementById('enter-artistPortfolio').click();
+        document.getElementById('enter-artistPortfolio').click(); // reloads page when info is change
     })
     .catch(error => {
         console.error('Error:', error); // Log error message
     });
 }
 
-// Modify triggerFileInput to accept slot parameter and pass it with the event
+//function to trigger file upload from user including genre and description boxes if used
 function triggerFileInput(slot) {
+    //checking for inputted media in the input box created for it
     const input = document.getElementById(`media-slot-${slot}-content`);
     input.addEventListener('change', function() {
         const file = input.files[0];
 
-        // Create FormData object to send file and other data
+        // Create FormData object to send file and any genre or desc if used
         const formData = new FormData();
         formData.append('username', sessionStorage.getItem('username'));
         formData.append('slot', slot);
         formData.append('file', file);
 
+        //only check if 1-5 media and only get genre and desc if not null
         if (slot > 0) {
             const genre = document.getElementById(`genre-input-${slot}`).value; // Use slot number to identify genre input
             const description = document.getElementById(`desc-input-${slot}`).value; // Use slot number to identify description input
@@ -332,7 +355,7 @@ function triggerFileInput(slot) {
         // Send POST request to upload API endpoint
         fetch('http://localhost:8080/ArtistPortfolio/api/uploadApi', {
             method: 'POST',
-            body: formData // Remove 'Content-Type' header and send FormData directly
+            body: formData 
         })
         .then(response => {
             if (response.ok) {
@@ -343,7 +366,7 @@ function triggerFileInput(slot) {
         })
         .then(responseText => {
             console.log(responseText); // Log success message
-            document.getElementById('enter-artistPortfolio').click();
+            document.getElementById('enter-artistPortfolio').click(); //reloads page when info is changed
         })
         .catch(error => {
             console.error('Error:', error); // Log error message
@@ -352,6 +375,7 @@ function triggerFileInput(slot) {
     input.click();
 }
 
+//function to trigger upload any Profile info(bio, location, occupation) takes in name of section being modified
 function triggerInfoInput(section) {
     const input = document.getElementById(`${section}-input`);
     const inputValue = input.value;
@@ -375,7 +399,7 @@ function triggerInfoInput(section) {
     })
     .then(responseText => {
         console.log(responseText); // Log success message
-        document.getElementById('enter-artistPortfolio').click();
+        document.getElementById('enter-artistPortfolio').click(); //creloading page when data is changed
     })
     .catch(error => {
         console.error('Error:', error); // Log error message
@@ -383,14 +407,14 @@ function triggerInfoInput(section) {
 }
 
 function resetArtistPortfolioView() {
-    // Remove all media slots
+    // Remove all media slot objects for reload
     for (let i = 1; i <= 5; i++) {
         const mediaSlot = document.getElementById(`slot-${i}`);
         mediaSlot.innerHTML = `<p><strong>Media Slot ${i}:</strong></p>
                                <input type="file" id="media-slot-${i}-content" style="display: none;">`;
     }
 
-    // Remove profile pic
+    // Remove profile pic for reload
     const profilePicSlot = document.getElementById('file-upload-0');
     profilePicSlot.innerHTML = '';
 
@@ -398,9 +422,14 @@ function resetArtistPortfolioView() {
     info.innerHTML = '';
 }
 
-//to be implemented later 
+//to be implemented later for users searcing you
+//copy of normal display with all input and delete objects taken away
 function displayOtherArtiste(portfolioInfo) {
     const infoSlot = document.getElementById(`info-upload`);
+
+    const infoParagraph = document.createElement('p');
+    infoParagraph.innerHTML = `<strong>${portfolioInfo[`username`]}`;
+    infoSlot.appendChild(infoParagraph);
 
     if (portfolioInfo[`occupation`]){
         const info = portfolioInfo[`occupation`];
