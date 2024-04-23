435b9e28d4f0ba8327352aaa74170b8a04c32a8d
Julie <97823380+Julie0126@users.noreply.github.com>
2024-04-22 19:39:02 -0700

    fix

--- src/Latest Frontend Project/TestUI/wwwroot/js/site.js
+++ src/Latest Frontend Project/TestUI/wwwroot/js/site.js
@@ -781,4 +781,8 @@
         window.location.href = 'InventoryStockView.html'; // Redirects the user to PriceRangeSorting.html
     });
 
+    //Artist Portfolio
+    document.getElementById('Artist Portfolio View').addEventListener('click', function () {
+        window.location.href = 'ArtistPortfolio.html'; // Redirects the user to PriceRangeSorting.html
+    });
 });
