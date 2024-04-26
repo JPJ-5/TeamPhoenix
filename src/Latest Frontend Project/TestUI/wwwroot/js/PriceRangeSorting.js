94c6f576a44be74fd86b20dd259c1ab83c59f81a
Parth Thanki <88216725+ParthThanki@users.noreply.github.com>
2024-04-25 23:37:25 -0700

    Update PriceRangeSorting.js

--- src/Latest Frontend Project/TestUI/wwwroot/js/PriceRangeSorting.js
+++ src/Latest Frontend Project/TestUI/wwwroot/js/PriceRangeSorting.js
@@ -115,22 +115,6 @@ function changePage(direction) {
 }
 
 function updateViewFormat() {
-    const results = document.getElementById('results');
-    const viewFormat = document.getElementById('viewFormat').value;
-
-    console.log(results);
-    console.log(viewFormat);
-
-    // Remove existing view classes
-    results.classList.remove('item-card-list', 'item-card-grid');
-
-    // Add the new view class based on the selected format
-    if (viewFormat === 'list') {
-        results.classList.add('item-card-list');
-    } else {
-        results.classList.add('item-card-grid');
-    }
-
     fetchItems(); // Reload items to display with the new format
 }
 
