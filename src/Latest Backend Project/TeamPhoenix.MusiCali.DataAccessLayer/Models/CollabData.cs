822f0050b66fe00d0a3805696e0b910693e8c3d8
Kihambo <48707537+Kihambo@users.noreply.github.com>
2024-05-01 16:25:16 -0700

    took off availUsers

--- src/Latest Backend Project/TeamPhoenix.MusiCali.DataAccessLayer/Models/CollabData.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.DataAccessLayer/Models/CollabData.cs
@@ -2,9 +2,7 @@ namespace TeamPhoenix.MusiCali.DataAccessLayer.Models{
 
 
 
-    public class CollabData{
-
-        public List<string> availUsers {get; set;}  
+    public class CollabData{ 
         public List<string> sentCollabs{ get; set; }
         public List<string> receivedCollabs{ get; set; }
         public List<List<string>> acceptedCollabs{ get; set; }
