2b3f2f87488e7d518f12d20cd69b2f5e234e605d
diego <72894762+meatballmyers@users.noreply.github.com>
2024-05-03 16:50:59 -0700

    Update LoginFrontendTesting.cy.js

--- src/Latest Frontend Project/cypress/e2e/LoginFrontendTesting.cy.js
+++ src/Latest Frontend Project/cypress/e2e/LoginFrontendTesting.cy.js
@@ -7,3 +7,13 @@ describe('register cases', () => {
         cy.get('#show-details-form').click();
     });
 });
+
+describe('login cases', () => {
+    it('should error when text fields are blank', () => {
+        cy.visit('http://localhost:8800/');
+
+        cy.get('#menu-btn').click();
+        cy.get('#show-login').click();
+        cy.get('#email-otp').click();
+    });
+});
