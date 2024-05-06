8fdfc8d9b210117d61da884e784b06445a80d685
vangriff00 <vankhuong0307@gmail.com>
2024-05-05 23:04:53 -0700

    Update InventoryStock Backend

--- src/Latest Backend Project/TeamPhoenix.MusiCali/Controllers/Seller Dynamic Dashboard Controller/InventoryStockController.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali/Controllers/Seller Dynamic Dashboard Controller/InventoryStockController.cs
@@ -2,15 +2,20 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
+using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+using TeamPhoenix.MusiCali.Logging;
+using TeamPhoenix.MusiCali.Security;
 [ApiController]
 [Route("SellerDashboard")]
 public class InventoryStockController : ControllerBase
 {
     private readonly InventoryStockService itemService;
+    private AuthenticationSecurity authentication;
 
     public InventoryStockController(IConfiguration configuration)
     {
         itemService = new InventoryStockService(configuration);
+        authentication = new AuthenticationSecurity(configuration);
     }
 
     [HttpGet("api/GetInventoryStock")]
@@ -18,9 +23,21 @@ public class InventoryStockController : ControllerBase
     {
         try
         {
-            var stockList = await itemService.RequestInventoryStockList(username);
+            var accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var role = authentication.getScopeFromToken(accessToken!);
+            var user = authentication.getUserFromToken(accessToken!);
+
+            if (!string.IsNullOrEmpty(role) && authentication.CheckIdRoleExisting(user, role))
+            {
+                var stockList = await itemService.RequestInventoryStockList(username);
+
+                return stockList.Count > 0 ? Ok(stockList) : NotFound("No stock items found.");
+            }
+            else
+            {
+                throw new Exception("Invalid Identity. Please Contact The System Administrator.");
+            }
             
-            return stockList.Count > 0 ? Ok(stockList) : NotFound("No stock items found.");
         }
         catch (Exception ex)
         {
