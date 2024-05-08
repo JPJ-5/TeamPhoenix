6c46d45f50ce202670f60a0bf07d76b6d0e8d094
vangriff00 <vankhuong0307@gmail.com>
2024-05-08 03:41:07 -0700

    Upload Backend for Item Deletion and Item Modification

---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.DataAccessLayer/ItemModificationDAO.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TeamPhoenix.MusiCali.DataAccessLayer
+{
+    public class ItemModificationDAO
+    {
+        private readonly string connectionString;
+        private readonly IConfiguration configuration;
+
+        public ItemModificationDAO(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.connectionString = this.configuration.GetSection("ConnectionStrings:ConnectionString").Value!;
+        }
+
+        public async Task<bool> ModifyItemTable(string creatorHash, ItemCreationModel model)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync(); // Open the MySQL connection.
+
+                    string commandText = "UPDATE CraftItem SET OfferablePrice = @OfferablePrice, Listed = @Listed";
+
+                    if(!(string.IsNullOrWhiteSpace(model.Name)))
+                    {
+                        commandText += ", Name = @Name";
+                    }
+                    if (model.Price >= 0m && model.Price <= 1000000m)
+                    {
+                        commandText += ", Price = @Price";
+                    }
+                    if(!string.IsNullOrWhiteSpace(model.Description))
+                    {
+                        commandText += ", Description = @Description";
+                    }
+                    if(model.StockAvailable >= 0)
+                    {
+                        commandText += ", StockAvailable = @StockAvailable";
+                    }
+                    if(model.ProductionCost >= 0 && model.ProductionCost <= 1000000m) {
+                        commandText += ", ProductionCost = @ProductionCost";
+                    }
+                    if (!string.IsNullOrWhiteSpace(model.SellerContact))
+                    {
+                        commandText += ", SellerContact = @SellerContact";
+                    }
+                    if (model.ImageUrls != null && model.ImageUrls.Contains("string"))
+                    {
+                        commandText += ", Image = @Image";
+                    }
+                    if (model.VideoUrls != null && model.VideoUrls.Contains("string"))
+                    {
+                        commandText += ", Video = @Video";
+                    }
+
+                    commandText += " WHERE SKU = @SKU AND CreatorHash = @CreatorHash";
+
+                    using (var command = new MySqlCommand(commandText, connection))
+                    {
+                        // Assign parameters to prevent SQL Injection.
+                        command.Parameters.AddWithValue("@OfferablePrice", model.OfferablePrice ? 1 : 0);
+                        command.Parameters.AddWithValue("@Listed", model.Listed ? 1 : 0);
+                        command.Parameters.AddWithValue("@SKU", model.Sku);
+                        command.Parameters.AddWithValue("@CreatorHash", creatorHash);
+
+
+
+                        if (!(string.IsNullOrWhiteSpace(model.Name)))
+                        {
+                            command.Parameters.AddWithValue("@Name", model.Name);
+                        }
+                        if (model.Price >= 0m && model.Price <= 1000000m)
+                        {
+                            command.Parameters.AddWithValue("@Price", model.Price);
+                        }
+                        if (!string.IsNullOrWhiteSpace(model.Description))
+                        {
+                            command.Parameters.AddWithValue("@Description", model.Description);
+                        }
+                        if (model.StockAvailable >= 0)
+                        {
+                            command.Parameters.AddWithValue("@StockAvailable", model.StockAvailable);
+                        }
+                        if (model.ProductionCost >= 0 && model.ProductionCost <= 1000000m)
+                        {
+                            command.Parameters.AddWithValue("@ProductionCost", model.ProductionCost);
+                        }
+                        if (!string.IsNullOrWhiteSpace(model.SellerContact))
+                        {
+                            command.Parameters.AddWithValue("@SellerContact", model.SellerContact);
+                        }
+                        if (model.ImageUrls != null && model.ImageUrls.Contains("string"))
+                        {
+                            command.Parameters.AddWithValue("@Image", string.Join(",", model.ImageUrls));
+                        }
+                        if (model.VideoUrls != null && model.VideoUrls.Contains("string"))
+                        {
+                            command.Parameters.AddWithValue("@Video", string.Join(",", model.VideoUrls));
+                        }
+                        int check = await command.ExecuteNonQueryAsync();
+
+                        if (check == 1)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            throw new Exception("No rows were affected.");
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                // Log MySQL exception
+                Console.WriteLine($"MySQL error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Log general exception
+                Console.WriteLine($"General error: {ex.Message}");
+            }
+
+            return false;
+        }
+
+
+        public async Task<bool> checkItemOwner(string creatorHash, string sku)
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync(); // Open the MySQL connection.
+                    string commandText = "SELECT CreatorHash FROM CraftItem WHERE SKU = @SKU";
+                    using (MySqlCommand command = new MySqlCommand(commandText, connection))
+                    {
+                        // Assign parameters to prevent SQL Injection.
+                        command.Parameters.AddWithValue("@SKU", sku);
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                string ownerHash = reader.GetString(0);
+                                if (creatorHash == ownerHash)
+                                {
+                                    return true;
+                                }
+                            }
+                            throw new Exception();
+                        }
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+            catch (Exception)
+            {
+                //  logging it
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.Services/ItemModificationService.cs
@@ -0,0 +1,193 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
+using Org.BouncyCastle.Bcpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamPhoenix.MusiCali.DataAccessLayer;
+using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+using TeamPhoenix.MusiCali.Logging;
+
+namespace TeamPhoenix.MusiCali.Services
+{
+    public class ItemModificationService
+    {
+        private readonly IConfiguration configuration;
+        private ItemModificationDAO itemModificationDAO;
+        private RecoverUserDAO recoverUserDAO;
+        private LoggerService loggerService;
+        private readonly string uploadFolderPath;
+        private ItemCreationService itemCreationService;
+        private IAmazonS3 s3Client;
+
+        public ItemModificationService(IAmazonS3 s3Client, IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            itemModificationDAO = new ItemModificationDAO(configuration);
+            recoverUserDAO = new RecoverUserDAO(configuration);
+            loggerService =  new LoggerService(configuration);
+            uploadFolderPath = configuration.GetValue<string>("AWS:UploadFolderPath");
+            itemCreationService = new ItemCreationService(s3Client, configuration);
+            this.s3Client = s3Client;
+        }
+
+        public async Task<bool> updateItemRequest(string username, ItemCreationModel model)
+        {
+            try {
+                var userHash = recoverUserDAO.GetUserHash(username);
+                if(userHash == null)
+                {
+                    throw new Exception("Invalid User Hash!");
+                }
+                //check if sku belong to userhash
+                //scan virus here
+                //validate the video and image name, type, size, extension
+
+                if(!(await itemModificationDAO.checkItemOwner(userHash, model.Sku!)))
+                {
+                    throw new Exception("No Item Found");
+                }
+
+
+                if(model.ImageUrls!.Contains("string") && model.VideoUrls!.Contains("string"))
+                {
+                    string userFolderPath = Path.Combine(uploadFolderPath, userHash);
+                    DirectoryInfo dir = new DirectoryInfo(userFolderPath);
+
+                    if (dir.Exists)
+                    {
+                        var deletePreviousFiles = DeleteAllFilesInFolderAsync(model.Sku!);
+                        if (!(await deletePreviousFiles))
+                        {
+                            throw new Exception("Unable To Remove Old Files");
+                        }
+                        List<string> uploadedUrls = await itemCreationService.UploadFolderFilesAsync(userHash, model.Sku!);          // upload file from sandbox to s3, return the file names.
+                        if (itemCreationService.DeleteSandboxFolder(userFolderPath))
+                        {
+                            Console.WriteLine("sucess delete the sandbox folder.");
+                        }
+                        else
+                        {
+                            throw new ArgumentException("fail to delete the sandbox.");
+                        }
+                        var imageExtensions = new HashSet<string> { ".jpg", ".png", ".gif", ".tiff" };
+                        var videoExtensions = new HashSet<string> { ".mp4", ".mov" };
+
+                        List<string> images = uploadedUrls.Where(f => imageExtensions.Contains(Path.GetExtension(f).ToLower())).ToList();
+                        List<string> videos = uploadedUrls.Where(f => videoExtensions.Contains(Path.GetExtension(f).ToLower())).ToList();
+                        model.ImageUrls = images;
+                        model.VideoUrls = videos;
+                    }
+                }
+                //string userFolderPath = Path.Combine(uploadFolderPath, userHash);
+                var validate = checkUpdateByte(model);
+                if (!validate)
+                {
+                    throw new Exception("Invalid Update Info!");
+                }
+                var requestStatus = await itemModificationDAO.ModifyItemTable(userHash, model);
+                if (!requestStatus)
+                {
+                    throw new Exception("Unable To Update Item");
+                }
+                var logSuccess = 
+                    loggerService.CreateLog(userHash, "Info", "Business", 
+                        $"ItemModification: {userHash} Sucessfully Modify {model.Sku}");
+                if(logSuccess.HasError == true)
+                {
+                    throw new Exception("Successfully Update Item But Unable To Log");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Result loggingError;
+                if (ex.Message == "Invalid User Hash!")
+                {
+                    loggingError = loggerService.CreateLog(null!, "Error", "Data", $"ItemModification: {ex.Message}");
+                }
+                else{
+                    loggingError = loggerService.CreateLog(recoverUserDAO.GetUserHash(username), "Error", "Server", $"ItemModification: {ex.Message}");
+                }
+                return false;
+            }
+        }
+
+        public bool checkUpdateByte(ItemCreationModel model)
+        {
+            // Check string lengths
+            if (model.Name.Length > 250 || (model.CreatorHash != null && model.CreatorHash.Length > 255) ||
+                (model.Sku != null && model.Sku.Length > 12) || model.SellerContact.Length > 300 ||
+                (model.ImageUrls!.Any(url => url.Length > 500)) ||
+                (model.VideoUrls!.Any(url => url.Length > 500)))
+            {
+                return false;
+            }
+            // Check decimal precision and scale
+            if (model.Price < 0m || model.Price > 1000000m || Decimal.Round(model.Price, 2) != model.Price ||
+                model.ProductionCost < 0m || model.ProductionCost > 1000000m || Decimal.Round(model.ProductionCost, 2) != model.ProductionCost)
+            {
+                return false;
+            }
+            // Check integers
+            if (model.StockAvailable < 0)
+            {
+                return false;
+            }
+            // If all checks are passed
+            return true;
+        }
+
+        public async Task<bool> DeleteAllFilesInFolderAsync(string sku)
+        {
+            if (string.IsNullOrEmpty(sku))
+            {
+                throw new ArgumentException("SKU must be provided.");
+            }
+
+            // Ensure the bucket exists
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(s3Client, uploadFolderPath);
+            if (!bucketExists)
+            {
+                throw new Exception($"Bucket {uploadFolderPath} does not exist");
+            }
+
+            // Construct the prefix to list objects within the SKU folder
+            string prefix = $"{sku.TrimEnd('/')}/";
+
+            // List all objects with the specified prefix (SKU folder)
+            var listRequest = new ListObjectsV2Request
+            {
+                BucketName = uploadFolderPath,
+                Prefix = prefix
+            };
+            ListObjectsV2Response listResponse;
+
+            try
+            {
+                do
+                {
+                    // Getting current batch of files
+                    listResponse = await s3Client.ListObjectsV2Async(listRequest);
+                    foreach (var obj in listResponse.S3Objects)
+                    {
+                        // Deleting each file found
+                        await s3Client.DeleteObjectAsync(uploadFolderPath, obj.Key);
+                    }
+                    // Set continuation token to continue listing files
+                    listRequest.ContinuationToken = listResponse.NextContinuationToken;
+                } while (listResponse.IsTruncated); // Continue while there are more files to be listed
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                loggerService.CreateLog(null, "Error", "Server", $"Failed to delete all files in folder {sku}: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
