62d2467b9a8182a4c8fcea44195b0c2ba00172b7
Kihambo <48707537+Kihambo@users.noreply.github.com>
2024-05-09 19:18:29 -0700

    added a space. that's it.

--- src/Latest Backend Project/TeamPhoenix.MusiCali.DataAccessLayer/Models/CollabData.cs
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.DataAccessLayer/Models/CollabData.cs
@@ -3,7 +3,7 @@ namespace TeamPhoenix.MusiCali.DataAccessLayer.Models{
 
 
     public class CollabData{ 
-        public List<string>?sentCollabs{ get; set; }
+        public List<string>? sentCollabs{ get; set; }
         public List<string>? receivedCollabs{ get; set; }
         public List<string>? acceptedCollabs{ get; set; }
     }
