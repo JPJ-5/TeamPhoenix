fa81b9475609ec567d441a818ae757d76be2824d
diego <72894762+meatballmyers@users.noreply.github.com>
2024-05-12 02:15:43 -0700

    Revert "Merge branch 'main' into diego-cypress"
    This reverts commit 3859548d269960aaf3712154c81ef45d14eae725, reversing
    changes made to 919da2310e6e1506f2670717dfebcc599265c0c5.

--- src/Latest Frontend Project/TestUI/wwwroot/ArtistPortfolioFeature/ArtistPortfolio.js
+++ src/Latest Frontend Project/TestUI/wwwroot/ArtistPortfolioFeature/ArtistPortfolio.js
@@ -29,7 +29,7 @@ function loadProfileData(username) {
             'Authentication': idToken,
             'Authorization': accessToken,
             'Content-Type': 'application/json',
-            'userName': username
+            'Username': username
         },
     })
         .then(response => {
