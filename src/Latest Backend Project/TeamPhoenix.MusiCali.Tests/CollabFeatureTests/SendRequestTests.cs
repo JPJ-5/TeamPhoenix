d26ca5083bce9a8aae1e24879f22086268371989
Kihambo <48707537+Kihambo@users.noreply.github.com>
2024-05-13 10:57:27 -0700

    added my tests

---
+++ src/Latest Backend Project/TeamPhoenix.MusiCali.Tests/CollabFeatureTests/SendRequestTests.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamPhoenix.MusiCali.DataAccessLayer;
+using TeamPhoenix.MusiCali.DataAccessLayer.Models;
+
+namespace TeamPhoenix.MusiCali.Tests
+{
+     [TestClass]
+     public class SendRequestTest
+     {
+
+        [TestMethod]
+        public async Task SearchUsers_ShouldReturnEmptyList_WhenNoMatchingUsersFound()
+        {
+            // Arrange
+            var query = "NonExistingUser";
+
+            // Act
+            var matchingUsers = CollabFeatureDAL.SearchUsers(query);
+
+            // Assert
+            Assert.AreEqual(0, matchingUsers.Count);
+        }
+
+        [TestMethod]
+        public void SearchUsers_ShouldReturnEmptyList_WhenQueryIsNull()
+        {
+            // Arrange
+            string query = null;
+
+            // Act
+            var matchingUsers = CollabFeatureDAL.SearchUsers(query);
+
+            // Assert
+            Assert.AreEqual(0, matchingUsers.Count);
+        }
+
+        [TestMethod]
+        public void SearchUsers_ShouldReturnEmptyList_WhenQueryIsEmpty()
+        {
+            // Arrange
+            var query = "";
+
+            // Act
+            var matchingUsers = CollabFeatureDAL.SearchUsers(query);
+
+            // Assert
+            Assert.AreEqual(0, matchingUsers.Count);
+        }
+
+        [TestMethod]
+        public void SearchUsersWithVisibility_ShouldReturnEmptyList_WhenNoMatchingUsersFound()
+        {
+            // Arrange
+            var query = "NonExistentUser";
+
+            // Act
+            var matchingUsers = CollabFeatureDAL.SearchUsers(query);
+
+            // Assert
+            Assert.AreEqual(0, matchingUsers.Count);
+        }
+
+        [TestMethod]
+        public void SearchUsersWithVisibility_ShouldReturnEmptyList_WhenQueryIsNull()
+        {
+            // Arrange
+            string query = null;
+
+            // Act
+            var matchingUsers = CollabFeatureDAL.SearchUsers(query);
+
+            // Assert
+            Assert.AreEqual(0, matchingUsers.Count);
+        }
+
+        [TestMethod]
+        public void SearchUsersWithVisibility_ShouldReturnEmptyList_WhenQueryIsEmpty()
+        {
+            // Arrange
+            var query = " ";
+
+            // Act
+            var matchingUsers = CollabFeatureDAL.SearchUsers(query);
+
+            // Assert
+            Assert.AreEqual(0, matchingUsers.Count);
+        }
+    }
+}
